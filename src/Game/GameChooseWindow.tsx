e415e333bc47d877d643b5549710ee735fbefffd
jonlld <87318928+jonlld@users.noreply.github.com>
2023-02-02 17:12:38 +0900

    Add transition and adjust layout of game choose screen

--- src/Game/GameChooseWindow.tsx
+++ src/Game/GameChooseWindow.tsx
@@ -1,43 +1,58 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { ChooseProps } from "../models/interfaces";
 
 const GameChooseWindow = ({ onStart }: ChooseProps): JSX.Element => {
+  const chooseContainerRef = useRef<HTMLDivElement>(null);
+
   const clickHandler = (e: React.MouseEvent<HTMLElement>) => {
     const type = e.currentTarget.getAttribute("value")!;
     onStart(type);
   };
 
+  // remove class on mount to trigger transition
+  useEffect(() => {
+    setTimeout(() => {
+      chooseContainerRef.current?.classList.remove("hidden");
+      chooseContainerRef.current?.classList.remove("shift-down");
+    }, 0);
+  }, []);
+
   return (
-    <section className="choose--container">
+    <section
+      ref={chooseContainerRef}
+      className="choose--container shift-down hidden"
+    >
       <h1 className="choose--header">Choose Game Type!</h1>
-      <button
-        className="btn btn__choose btn__add"
-        value="add"
-        onClick={clickHandler}
-      >
-        Addition
-      </button>
-      <button
-        className="btn btn__choose btn__subtract"
-        value="subtract"
-        onClick={clickHandler}
-      >
-        Subtraction
-      </button>
-      <button
-        className="btn btn__choose btn__multiply"
-        value="multiply"
-        onClick={clickHandler}
-      >
-        Multiplication
-      </button>
-      <button
-        className="btn btn__choose btn__random"
-        value="random"
-        onClick={clickHandler}
-      >
-        Random!
-      </button>
+      <div className="choose--buttons">
+        <button
+          className="btn btn__choose btn__add"
+          value="add"
+          onClick={clickHandler}
+        >
+          Addition
+        </button>
+        <button
+          className="btn btn__choose btn__subtract"
+          value="subtract"
+          onClick={clickHandler}
+        >
+          Subtraction
+        </button>
+        <button
+          className="btn btn__choose btn__multiply"
+          value="multiply"
+          onClick={clickHandler}
+        >
+          Multiplication
+        </button>
+        <button
+          className="btn btn__choose btn__random"
+          value="random"
+          onClick={clickHandler}
+        >
+          Random!
+        </button>
+      </div>
     </section>
   );
 };
