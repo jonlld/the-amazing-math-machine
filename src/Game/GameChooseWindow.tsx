cf10ec63b4d9f6970fc624697521f8ca621c3ccf
jonlld <87318928+jonlld@users.noreply.github.com>
2023-02-03 12:59:39 +0900

    Minor adjustments and change button text on choose game

--- src/Game/GameChooseWindow.tsx
+++ src/Game/GameChooseWindow.tsx
@@ -22,35 +22,35 @@ const GameChooseWindow = ({ onStart }: ChooseProps): JSX.Element => {
       ref={chooseContainerRef}
       className="choose--container shift-down hidden"
     >
-      <h1 className="choose--header">Choose Game Type!</h1>
+      <h1 className="choose--header">Choose Game Type:</h1>
       <div className="choose--buttons">
         <button
           className="btn btn__choose btn__add"
           value="add"
           onClick={clickHandler}
         >
-          Addition
+          Add +
         </button>
         <button
           className="btn btn__choose btn__subtract"
           value="subtract"
           onClick={clickHandler}
         >
-          Subtraction
+          Subtract -
         </button>
         <button
           className="btn btn__choose btn__multiply"
           value="multiply"
           onClick={clickHandler}
         >
-          Multiplication
+          Multiply *
         </button>
         <button
           className="btn btn__choose btn__random"
           value="random"
           onClick={clickHandler}
         >
-          Random!
+          Mix it up!
         </button>
       </div>
     </section>
